db7e53ead0223f1735889d3625999f8b085c2f22
ehenko.97@mail.ru <ehenko.97@mail.ru>
2024-07-07 18:22:33 +0300

    добавил Name

--- main_test.go
+++ main_test.go
@@ -1,38 +1,29 @@
 package main
 
 import (
-	"io/ioutil"
 	"testing"
 )
 
-// Структура для хранения тестовых данных
-type testData struct {
-	fileName string
-	expected string
-}
-
 func TestMaskLink(t *testing.T) {
-	tests := map[string]testData{
-		"Test 1": {"testdata/test1.txt", "Check this http://****************"},
-		"Test 2": {"testdata/test2.txt", "No link here"},
-		"Test 3": {"testdata/test3.txt", "Multiple links http://*********** and http://********"},
-		"Test 4": {"testdata/test4.txt", "Edge case http://"},
-		"Test 5": {"testdata/test5.txt", "Trailing http://*********** and some text"},
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"Mask1", "http://example.com", "http://***********"},
+		{"Mask2", "no http link here", "no http link here"},
+		{"Mask3", "http://example.com and some text", "http://*********** and some text"},
+		{"Mask4", "https://secure.com should not be masked", "https://secure.com should not be masked"},
+		{"Mask5", "http://", "http://"},
+		{"Mask6", "", ""},
 	}
 
-	for name, test := range tests {
-		// Чтение содержимого файла тестовых данных
-		content, err := ioutil.ReadFile(test.fileName)
-		if err != nil {
-			t.Fatalf("Failed to read test data file '%s': %s", test.fileName, err)
-		}
-
-		byteSlice := content
+	for _, test := range tests {
+		byteSlice := []byte(test.input)
 		maskLink(byteSlice, []byte("http://"))
-		output := string(byteSlice)
-
-		if output != test.expected {
-			t.Errorf("[%s] Expected '%s', but got '%s'", name, test.expected, output)
+		result := string(byteSlice)
+		if result != test.expected {
+			t.Errorf("Test case %q: maskLink(%q) = %q; want %q", test.name, test.input, result, test.expected)
 		}
 	}
 }
